1491e1889c25dff2c8656660cfaeb7bc51c10a33
Igor Veyner <igorveyner95@gmail.com>
2020-06-16 20:40:41 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,7 @@
 def speak_to_grandma(phrase)
   if(phrase != phrase.upcase)
     return "HUH?! SPEAK UP, SONNY!"
-  else if 
   elsif(phrase == "I LOVE YOU GRANDMA!")
+    return "I LOVE YOU TOO PUMPKIN"
   end
 end
